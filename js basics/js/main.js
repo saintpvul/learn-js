91400afce1a0f33c02755f6e531c02b067f4e221
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-08-23 02:44:23 +0600

    feat: complete js fundamental func ex_3

--- js basics/js/main.js
+++ js basics/js/main.js
@@ -8,5 +8,5 @@ CAll alert
 
 // Solution
 
-let ourPlanetName = "Earth planet";
-let currentVisitor = "Current visitor";
+const BIRTHDAY = "18.04.1982"; // birthday is const value
+const age = somecode(BIRTHDAY); // we can change code in age var
