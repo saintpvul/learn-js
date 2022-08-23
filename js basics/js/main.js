b4f9628df5170b86f9ca7c2439738797ded99979
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-08-24 01:34:24 +0600

    feat: complete js fundamentals base operators ex 1

--- js basics/js/main.js
+++ js basics/js/main.js
@@ -2,11 +2,14 @@
 
 /* 
 
-interaction
+base operators
 
 */
 
 // Solution
 
-let name = prompt("Enter your name: ", "");
-alert(name);
+let a = 1,
+  b = 1;
+
+let c = ++a; // c 2, a 2
+let d = b++; // d 1, b 2
