6dc19e079cdf792a884cf6043ce58ff974ee1730
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-08-25 04:16:01 +0600

    feat: complete js fundamentals Conditional branching ex1

--- js basics/js/main.js
+++ js basics/js/main.js
@@ -2,16 +2,12 @@
 
 /* 
 
-comparision operators
+Conditional branching
 
 */
 
 // Solution
 
-5 > 4; // true
-"ананас" > "яблоко"; // false
-"2" > "12"; // true
-undefined == null; // true
-undefined === null; // false
-null == "\n0\n"; // false
-null === +"\n0\n"; // false
+if ("0") {
+  alert("Привет");
+} // true
