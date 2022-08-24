833dfb915f1c05404d01678f13e69dbcb72d2645
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-08-25 04:54:12 +0600

    feat: complete js fundamentals logical operators ex7

--- js basics/js/main.js
+++ js basics/js/main.js
@@ -9,5 +9,10 @@ logical operators
 // Solution
 
 let age = prompt("enter your age");
-if (age >= 14 && age <= 90) alert(" thats work");
-else alert("is it your age?");
+if (!(age >= 14 && age <= 90)) alert("your age is outside the range");
+else alert("your age is inside range");
+
+let age1 = prompt("enter your age1");
+if (age1 < 14 || age1 > 90) {
+  alert("true");
+} else alert("false");
