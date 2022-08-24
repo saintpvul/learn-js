44e3624918d44098a026cfe2c01bc33e1305f0cf
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-08-25 04:45:11 +0600

    feat: complete js fundamentals logical operators ex6

--- js basics/js/main.js
+++ js basics/js/main.js
@@ -8,4 +8,6 @@ logical operators
 
 // Solution
 
-alert(null || (2 && 3) || 4); // 3
+let age = prompt("enter your age");
+if (age >= 14 && age <= 90) alert(" thats work");
+else alert("is it your age?");
