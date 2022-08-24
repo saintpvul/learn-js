72ccecc86cd23e98c469c705491ed8f1e43cb4ed
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-08-25 04:23:48 +0600

    feat: complete js fundamentals Conditional branching ex4

--- js basics/js/main.js
+++ js basics/js/main.js
@@ -8,13 +8,5 @@ Conditional branching
 
 // Solution
 
-let ask = prompt("Enter any number");
-if (ask > 0) {
-  alert(1);
-} else if (ask < 0) {
-  alert(-1);
-} else if (ask == 0) {
-  alert("0");
-} else {
-  alert("it is not a number i guess");
-}
+result = a + b < 4 ? "less than" : "more than";
+alert(result);
