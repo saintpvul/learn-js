8fded6c9a3c67ed86892be1738aca567e00cbcc8
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-08-25 05:11:16 +0600

    feat: complete js Nullish coalescing operator start

--- js basics/js/main.js
+++ js basics/js/main.js
@@ -2,23 +2,8 @@
 
 /* 
 
-logical operators
+Nullish coalescing operator
 
 */
 
 // Solution
-
-let login = prompt("enter your login: ");
-if (login === "admin") {
-  let password = prompt("enter your pass:");
-
-  if (password === "imaboss") {
-    alert("Hello");
-  } else if (password === "" || password === null) {
-    alert("canceled");
-  } else alert("wrong pass");
-} else if (login === "" || login === null) {
-  alert("canceled");
-} else {
-  alert("i don't know you");
-}
