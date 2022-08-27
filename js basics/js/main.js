afeae1127c1bec4f2bf8dc899fc95acc41deb5a3
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-08-27 17:52:00 +0600

    feat: complete js while / for loops ex2_1

--- js basics/js/main.js
+++ js basics/js/main.js
@@ -8,9 +8,6 @@ While / for
 
 // Solution
 
-let i = 3;
-
-while (i) {
-  alert(i--);
-}
-// 1 / zero is false
+let i = 0;
+while (++i < 5) alert(i);
+// 4 / (1 + i) < 5  four times will be true. When 1 + i == 5 iteration will be break
