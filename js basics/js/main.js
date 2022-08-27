7556629b0df819c9e6bf2fc9283ce9e89c2c1bf3
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-08-27 18:13:30 +0600

    feat: start js switch construction

--- js basics/js/main.js
+++ js basics/js/main.js
@@ -2,17 +2,8 @@
 
 /* 
 
-While / for
+switch construction
 
 */
 
 // Solution
-
-let prime = 10;
-
-nextPrime: for (let i = 2; i <= prime; i++) {
-  for (let j = 2; j < i; j++) {
-    if (i % j == 0) continue nextPrime;
-  }
-  console.log(i);
-}
