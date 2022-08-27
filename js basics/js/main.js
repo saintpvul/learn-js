4718d6da6fef6f4bbca6a21a5033e55922d22cf9
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-08-27 17:59:33 +0600

    feat: complete js while / for loops ex3_2

--- js basics/js/main.js
+++ js basics/js/main.js
@@ -8,5 +8,5 @@ While / for
 
 // Solution
 
-for (let i = 0; i < 5; i++) alert(i);
-// print 0 1 2 3 4 and then stops 'cause 5 is equal to 5 and is not true
+for (let i = 0; i < 5; ++i) alert(i);
+//  does the same thing cause we check if the i == true.
