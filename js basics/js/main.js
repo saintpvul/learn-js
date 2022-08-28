65ab1f70794f07850ae2e7c66d4684e29084704e
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-08-29 00:05:03 +0600

    feat: complete js functions ex1_1

--- js basics/js/main.js
+++ js basics/js/main.js
@@ -7,3 +7,13 @@ functins
 */
 
 // Solution
+
+function checkAge(age) {
+  if (age > 18) {
+    return true;
+  } else {
+    // ...
+    return confirm("Родители разрешили?");
+  }
+}
+// if true will return logical value true / if not true return string
