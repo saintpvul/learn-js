c86a267891ad78a4871b11f9e7f4581ff746b2a4
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-08-28 21:32:52 +0600

    feat: complete js switch construction ex1

--- js basics/js/main.js
+++ js basics/js/main.js
@@ -7,3 +7,16 @@ switch construction
 */
 
 // Solution
+
+if (browser === "Edge") {
+  alert("You've got the Edge");
+} else if (
+  browser === "Chrome" ||
+  browser === "Firefox" ||
+  browser === "Safari" ||
+  browser === "Opera"
+) {
+  alert("okay we suppert these browsers too");
+} else {
+  alert("We hope that this page looks ok!");
+}
