1db2e4e6e82c1416a5201f38338b408933f4eaa1
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-08-28 21:36:07 +0600

    feat: complete js switch construction ex2

--- js basics/js/main.js
+++ js basics/js/main.js
@@ -8,15 +8,18 @@ switch construction
 
 // Solution
 
-if (browser === "Edge") {
-  alert("You've got the Edge");
-} else if (
-  browser === "Chrome" ||
-  browser === "Firefox" ||
-  browser === "Safari" ||
-  browser === "Opera"
-) {
-  alert("okay we suppert these browsers too");
-} else {
-  alert("We hope that this page looks ok!");
+const number = +prompt("Enter number from 0 to 3:", "");
+switch (number) {
+  case 0:
+    alert("You enter 0");
+    break;
+
+  case 1:
+    alert("You enter 1");
+    break;
+
+  case 2:
+  case 3:
+    alert("You enter 2, or maybe 3");
+    break;
 }
