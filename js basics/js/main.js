ac71ff1d28e684b9851a123e4176861d6dcd60e5
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-08-29 00:16:51 +0600

    feat: complete js functions ex4

--- js basics/js/main.js
+++ js basics/js/main.js
@@ -8,6 +8,20 @@ functins
 
 // Solution
 
-function min(a, b) {
-  return a < b ? a : b;
+function pow(x, n) {
+  let result = x;
+
+  for (let i = 0; i < n; i++) {
+    result *= x;
+  }
+  return result;
+}
+
+let x = +prompt("x?", "");
+let n = +prompt("n?", "");
+
+if (n < 1) {
+  alert("you have to use natural numbers");
+} else {
+  alert(pow(x, n));
 }
