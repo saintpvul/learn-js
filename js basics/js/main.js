39c10da5b1d88af546da56405db3b4fb65885259
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-08-28 21:36:45 +0600

    feat: start js functions

--- js basics/js/main.js
+++ js basics/js/main.js
@@ -2,24 +2,8 @@
 
 /* 
 
-switch construction
+functins
 
 */
 
 // Solution
-
-const number = +prompt("Enter number from 0 to 3:", "");
-switch (number) {
-  case 0:
-    alert("You enter 0");
-    break;
-
-  case 1:
-    alert("You enter 1");
-    break;
-
-  case 2:
-  case 3:
-    alert("You enter 2, or maybe 3");
-    break;
-}
