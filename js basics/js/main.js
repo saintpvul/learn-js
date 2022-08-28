7524ea7c0e8c3a12f8556af6b34407401f40cb03
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-08-29 00:50:20 +0600

    feat: start js function expression

--- js basics/js/main.js
+++ js basics/js/main.js
@@ -2,26 +2,8 @@
 
 /* 
 
-functins
+function expression
 
 */
 
 // Solution
-
-function pow(x, n) {
-  let result = x;
-
-  for (let i = 0; i < n; i++) {
-    result *= x;
-  }
-  return result;
-}
-
-let x = +prompt("x?", "");
-let n = +prompt("n?", "");
-
-if (n < 1) {
-  alert("you have to use natural numbers");
-} else {
-  alert(pow(x, n));
-}
