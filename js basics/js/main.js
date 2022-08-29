933d6900466da35be331d176c81c8ed620d78811
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-08-29 23:24:24 +0600

    feat: complete js arrow funcrions basics ex1

--- js basics/js/main.js
+++ js basics/js/main.js
@@ -7,3 +7,13 @@ Arrow functions, the basics
 */
 
 // Solution
+
+function ask(question, yes, no) {
+  if (confirm(question)) yes();
+  else no();
+}
+ask(
+  "Do you agree?",
+  () => alert("Agreed"),
+  () => alert("Canceled")
+);
