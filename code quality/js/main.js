25a3d81045ba6f38eb01e6a3653953ec9794be4d
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-08-31 20:56:56 +0600

    update: start js code quality Polyfills and transpilers

--- code quality/js/main.js
+++ code quality/js/main.js
@@ -2,37 +2,8 @@
 
 /* 
 
-Automated testing with Mocha start
+Polyfills and transpilers
 
 */
 
 // Solution
-
-it("raises x to the power n", function () {
-  let x = 5;
-
-  let result = x;
-  assert.equal(pow(x, 1), result);
-
-  result *= x;
-  assert.equal(pow(x, 2), result);
-
-  result *= x;
-  assert.equal(pow(x, 3), result);
-});
-
-//all test inserts in one it block. hard to test separate case.
-
-describe("raises x to the power n", function () {
-  it("5 in the power 1 will be 5", function () {
-    assert.equal(pow(5, 1), 5);
-  });
-
-  it("5 in the power 2 will be 25", function () {
-    assert.equal(pow(5, 2), 25);
-  });
-
-  it("5 in the power 3 will be 125", function () {
-    assert.equal(pow(5, 3), 125);
-  });
-});
