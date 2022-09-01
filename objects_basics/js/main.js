4de4d2993d8f780786bf7b6620a3a35431e2f6af
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-09-01 22:26:09 +0600

    feat:complete learn-js object-basics ex2

--- objects_basics/js/main.js
+++ objects_basics/js/main.js
@@ -8,15 +8,13 @@ Objects
 
 // Solution
 
-//1
-const user = {};
-//2
-user.name = "John";
-//3
-user.surname = "Smith";
-//4
-user.name = "Pete";
-//5
-delete user.name;
+function isEmpty(obj) {
+  for (let key in obj) {
+    return false;
+  }
+  return true;
+}
 
-console.log(user);
+let schedule = {};
+
+console.log(isEmpty(schedule));
