67ad453c080a4b4d8e832963456ae5ea80d204a1
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-09-01 22:40:55 +0600

    update: start learn-js objacts-basics object references and copying

--- objects_basics/js/main.js
+++ objects_basics/js/main.js
@@ -2,26 +2,8 @@
 
 /* 
 
-Objects
+object references and copying
 
 */
 
 // Solution
-
-let menu = {
-  width: 200,
-  height: 300,
-  title: "My menu",
-};
-
-function multiplyNumeric(obj) {
-  for (let key in obj) {
-    if (typeof obj[key] === "number") {
-      obj[key] *= 2;
-    }
-  }
-}
-
-multiplyNumeric(menu);
-
-console.log(menu);
