e761686188753d6dc3e6749d3ef0fe38438af6c5
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-09-01 22:31:53 +0600

    feat:complete learn-js object-basics ex4

--- objects_basics/js/main.js
+++ objects_basics/js/main.js
@@ -8,10 +8,15 @@ Objects
 
 // Solution
 
-const user = {
-  name: "John",
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130,
 };
 
-//
-user.name = "Pete";
-// the name key would be changed cause we don't change variable
+let sum = 0;
+for (let prop in salaries) {
+  sum += salaries[prop];
+}
+
+console.log(sum);
