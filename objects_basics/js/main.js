17c9b72701594cf48b2ae73cb747dea15e2583a3
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-09-01 22:39:18 +0600

    feat:complete learn-js object-basics ex5

--- objects_basics/js/main.js
+++ objects_basics/js/main.js
@@ -8,15 +8,20 @@ Objects
 
 // Solution
 
-let salaries = {
-  John: 100,
-  Ann: 160,
-  Pete: 130,
+let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu",
 };
 
-let sum = 0;
-for (let prop in salaries) {
-  sum += salaries[prop];
+function multiplyNumeric(obj) {
+  for (let key in obj) {
+    if (typeof obj[key] === "number") {
+      obj[key] *= 2;
+    }
+  }
 }
 
-console.log(sum);
+multiplyNumeric(menu);
+
+console.log(menu);
