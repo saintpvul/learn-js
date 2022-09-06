0928b141ea0503cb4b676e6fa51a4adef4c7c03b
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-09-07 05:38:19 +0600

    feat: complete learn-js object methods this ex 1

--- objects_basics/js/main.js
+++ objects_basics/js/main.js
@@ -7,3 +7,16 @@ Object methods, "this"
 */
 
 // Solution
+
+function makeUser() {
+  return {
+    name: "John",
+    ref: this,
+  };
+}
+
+let user = makeUser();
+
+alert(user.ref.name);
+
+// error. this is undefined
