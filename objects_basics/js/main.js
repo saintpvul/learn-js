7c464e6a33e86cf328e081b074b8b50bfc49c484
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-09-07 21:55:37 +0600

    init: start learn-js Constructor, operator new

--- objects_basics/js/main.js
+++ objects_basics/js/main.js
@@ -2,26 +2,8 @@
 
 /* 
 
-Object methods, "this"
+Constructor, operator "new"
 
 */
 
 // Solution
-
-let ladder = {
-  step: 0,
-  up() {
-    this.step++;
-    return this;
-  },
-  down() {
-    this.step--;
-    return this;
-  },
-  showStep: function () {
-    alert(this.step);
-    return this;
-  },
-};
-
-ladder.up().up().down().showStep().down().showStep(); // must show 1 then 0
