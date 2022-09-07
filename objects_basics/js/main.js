13fef994d9e2e6d26508803498f967b6fea26865
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-09-08 00:58:41 +0600

    init: start learn-js optional chaining ?.

--- objects_basics/js/main.js
+++ objects_basics/js/main.js
@@ -2,23 +2,8 @@
 
 /* 
 
-Constructor, operator "new"
+Optional chaining '?.'
 
 */
 
 // Solution
-
-function Accumulator(startingValue) {
-  this.value = startingValue;
-
-  this.read = function () {
-    this.value += +prompt("Enter next num: ", 0);
-  };
-}
-
-let accumulator = new Accumulator(1);
-
-accumulator.read();
-accumulator.read();
-
-alert(accumulator.value);
