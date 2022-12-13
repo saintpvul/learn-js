79a6cee848aafcd66dd2bcbed7ff77edf1b86566
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-12-13 20:55:21 +0600

    init: complete test case for methods of primitives on learnjs web portal

--- data types/js/main.js
+++ data types/js/main.js
@@ -9,3 +9,12 @@ Methods of primitives
 // Solution
 
 // start data types section
+
+let str = "Привет";
+
+str.test = 5;
+
+alert(str.test);
+// solution
+//error in strict mode.
+// undefined in no strict mode
